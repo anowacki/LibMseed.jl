0463f8a5470bae566459db47a884c87d6c89c865
Andy Nowacki <a.nowacki@leeds.ac.uk>
2022-03-29 20:55:47 +0100

    Add more tests, starting with IO

---
+++ test/io.jl
@@ -0,0 +1,28 @@
+using Test
+using LibMseed
+
+testfile(path...) = joinpath(@__DIR__, "..", "lib", "libmseed", "test", "data", path...)
+
+error_files = (
+    "corrupt-blockettes-wrongnext.mseed",
+    "detection.record.mseed",
+    "text-encoded.mseed",
+)
+
+@testset "File/buffer reading" begin
+    @testset "Errors" begin
+        @testset "$file" for file in error_files
+            @test_throws ErrorException LibMseed.read_file(testfile(file))
+            @test_throws ArgumentError LibMseed.read_buffer(read(testfile(file)))
+        end
+    end
+
+    @testset "File v buffer" begin
+        @testset "$filename" for filename in collect(f for f in readdir(testfile()) if !(f in error_files))
+            file = testfile(filename)
+            mf = LibMseed.read_file(file)
+            mb = LibMseed.read_buffer(read(file))
+            @test mf == mb
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using LibMseed
+using Test
 
-data = read("/Users/nowacki/Work/Projects/Seis.jl/test/test_data/io/miniseed_GB.CWF.single_sample_gaps.mseed")
-
-println(LibMseed.read_buffer(data))
+@testset "All tests" begin
+    include("io.jl")
+end
