bff0703ad784296b2199d01f7a66194dc02cc75e
Andy Nowacki <a.nowacki@leeds.ac.uk>
2022-03-30 13:06:30 +0100

    Add `write_data` to write a single record to a miniSEED file
    - By default, write miniSEED version 2 because lots of software
      doesn't understand version 3 yet.
    - There appears to be an issue with using Steim (-1 or -2)
      encoding; files written have an incorrect checksum.  It's not clear
      where the bug is coming from so for now Steim encoding is not
      supported.

--- src/high_level.jl
+++ src/high_level.jl
@@ -530,6 +530,142 @@ function read_file(file; verbose_level=0)
 end
 
 """
+    write_data(file, data, sample_rate, starttime, id; append=false, verbose_level=0, compress=:steim2, pubversion=1, record_length=nothing) -> n
+
+Write `data` in miniSEED format to `file`.  The number of samples per second
+is given by `sample_rate`, and `starttime` is the date of the first sample.
+
+`id` is an ASCII string giving the ID of the trace.  If it is longer than
+$LM_SIDLEN characters, it is truncated with a warning.
+
+Use `append=true` to add new records on to `file`; otherwise any existing
+file is overwritten.  `file` is created if it does not already exist in
+either case.
+
+`verbose_level` is passed to the `libmseed` routine `mstl3_readtracelist`
+to control the verbosity level, with `0` (the default) only writing
+error messages to stderr, and higher numbers causing more information
+to be printed.
+
+# Allowable `data` element types
+miniSEED files can only contain data for `Int32`, `Float32` or `Float64`
+element types.  Therefore, this function will only write vectors with
+one of these element types and throw an error if a different type is
+passed in.  You should convert data to one of these types before calling
+this function.
+
+# Compression options
+`compress` can take the values `:steim1` or `:steim2`, causing the file
+to be written respectively with Steim-1 or Steim-2 compression.  Note that
+this is only possible for `Int32` data.
+
+# All keyword arguments
+- `append = false`: If `true`, append this data to an existing file if it exists.
+- `verbose_level = 0`: Control verbosity of the libmseed library, with
+  values greater than 0 increasing verbosity.
+- `compress = :steim2`: If input data are integers, use Steim compression
+  when writing the data.  Options are: `:steim1` (Steim-1 compression)
+  and `:steim2` (Steim-2) compression.
+- `pubversion = 1`: Set the publication version for the record.  In
+  SEED, later versions correspond to revised data and are read from
+  files in preference to earlier versions.
+- `record_length = nothing`: Control the record length of records written
+  to disk.  The default value (`-1`) allows the libmseed library to
+  determine the record length.
+- `version = 2`: miniSEED file version to write.  Note that little other
+  software supports the current version `3`, so the default is to write
+  version `2`.
+"""
+function write_data(file, data, sample_rate, starttime, id;
+        append::Bool=false, verbose_level::Integer=0,
+        compress::Union{Nothing,Symbol}=nothing,
+        pubversion::Integer=1, record_length::Union{Nothing,Integer}=nothing,
+        version::Integer=2)
+
+    # TODO: Investigate and fix issue where Steim compression produces
+    #       files with an incorrect checksum and sometimes corrupt data
+    #       when read by obspy, SeisIO and SAC (but not by libmseed!).
+    #       For now, disallow Steim encoding entirely.
+    compress !== nothing &&
+        throw(ArgumentError(
+            "Steim compression is not currently supported due to a bug"))
+
+    # Check arguments
+    compress in (nothing, :steim1, :steim2) ||
+        throw(ArgumentError("unrecognised option for `compress`"))
+    record_length !== nothing && (record_length < -1 || record_length == 0) &&
+        throw(ArgumentError("unknown value for `record_length` (must be -1 or > 0)"))
+    2 <= version <= 3 ||
+        @warn("miniSEED format versions other than 2 and 3 may not work")
+
+    record = C_NULL
+    reclen = record_length === nothing ? -1 : record_length
+    swapflag = 0
+    if length(id) > LM_SIDLEN
+        @warn("station id truncated to first $LM_SIDLEN characters")
+    end
+    sid = string2bytes(NTuple{LM_SIDLEN,UInt8}, id)
+    formatversion = version
+    flags = MSF_FLUSHDATA
+    starttime = NanosecondDateTime(starttime)
+    samprate = sample_rate
+    samplecnt = -1
+    crc = 0
+    extralength = 0
+    datalength = 0
+    extra = C_NULL
+    datasamples = pointer(data)
+    datasize = sizeof(eltype(data))*length(data)
+    numsamples = length(data)
+    sampletype = sample_code(data)
+    
+    # Determine default encoding
+    encoding = _encoding(data)
+
+    # Allow compression if requested and using `Int32`s
+    if eltype(data) == Int32 && compress !== nothing
+        encoding = if compress === :steim1
+            DE_STEIM1
+        elseif compress === :steim2
+            DE_STEIM2
+        end
+    end
+
+    msr = Ref(MS3Record(record, reclen, swapflag, sid, formatversion, flags,
+        starttime, samprate, encoding, pubversion, samplecnt, crc,
+        extralength, datalength, extra, datasamples, datasize, numsamples,
+        sampletype))
+
+    overwrite = append ? 0 : 1
+
+    err = ccall(
+        (:msr3_writemseed, libmseed),
+        Int64,
+        (Ptr{MS3Record}, Cstring, Int8, UInt32, Int8),
+        msr, file, overwrite, flags, verbose_level
+    )
+
+    if err == -1
+        error("error writing records")
+    end
+
+    # If not an error, this is the number of records written
+    err
+end
+
+_encoding(::AbstractVector{<:Int32}) = DE_INT32
+_encoding(::AbstractVector{<:Float32}) = DE_FLOAT32
+_encoding(::AbstractVector{<:Float64}) = DE_FLOAT64
+_encoding(_) = -1
+
+"Return the sample type character code given the data type"
+sample_code(::Type{Int32}) = 'i'
+sample_code(::Type{Float32}) = 'f'
+sample_code(::Type{Float64}) = 'd'
+sample_code(::AbstractVector{<:T}) where T = sample_code(T)
+sample_code() = error("unsupported data type $T for writing")
+
+"""
     init_tracelist(; verbose=false) -> mstl::Ptr{libmseed._MS3TraceList}
 
 Create a new `_MS3TraceList`, and return a pointer to it.
